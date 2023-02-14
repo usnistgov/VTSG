26156393979a9fa1a70ce48dc71e2002f9353a2e
Paul E. Black <drpaule@gmail.com>
2023-02-14 17:59:46 -0500

    Add CWE23 Path Traversal to Python
    The weakness is in the sink.  The possible unprotected input is a filter so it can be
    wrapped in complexities.

---
+++ tests/Python/Other/CWE23/unsafe/CWE23__I_InitNone__F_console_input__S_path_traversal__1-70b.py
@@ -0,0 +1,20 @@
+
+'''
+Initialize to None
+input (filter): console << INPUT
+../etc/passwd
+INPUT
+sink: open file named by input
+'''
+
+
+class Class_1:
+    def __init__(self,tainted_2_1):
+        self.var_1 = tainted_2_1
+    def  get_var_1(self):
+
+        tainted_2 = var_1
+
+        tainted_3 = input() # read one line
+
+        return tainted_3
