4356067b179189ed9b5728f836b860a230b58eca
Paul E. Black <drpaule@gmail.com>
2023-04-19 10:58:38 -0400

    Add a second condition for test025 to test storing test cases.

---
+++ tests/test025/Zarahemla/CWE_22/safe/CWE_22__I_args__F_remove_bad_chars_path__S_ls__EQ_pass__1-1.3.py
@@ -0,0 +1,43 @@
+'''
+input: command line args
+filter: remove all potentially dangerous characters from path
+sink: run ls in a dir
+exec_query: make sure EQ is used at least once in tests
+'''
+'''
+Created by Paul E. Black and William Mentzer 2020
+
+This software was developed at the National Institute of Standards and Technology
+by employees of the Federal Government in the course of their official duties.
+Pursuant to title 17 Section 105 of the United States Code the software is not
+subject to copyright protection and are in the public domain.
+
+We would appreciate acknowledgment if the software is used.
+
+Paul E. Black  paul.black@nist.gov
+William Mentzer willmentzer20@gmail.com
+
+'''
+import os
+import re
+import sys
+
+
+def main():
+    tainted_2 = sys.argv[1]
+    tainted_3 = tainted_2
+
+    if 4+2<=42:
+
+        # remove any other characters
+        pattern = r'[^a-zA-Z0-9_ ,/]'
+        # lstrip removes any leading /
+        tainted_3 = re.sub(pattern, '', tainted_2).lstrip('/')
+
+
+
+    os.system('ls ' + tainted_3);
+
+    pass
+if __name__ == '__main__':
+        main()
