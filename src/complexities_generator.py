776af596027dcecbf229fe1da70944d759cde6c1
Paul E. Black <drpaule@gmail.com>
2024-02-05 16:33:26 -0500

    Internal code improvements in complexities_generator.py.
    Create a new (local) class: Complexity Instances.  Previously an "open coded" list of
    complexity instance information was created each time.  I figured out and documented
    what each piece of information is.  The code now uses accessor functions.
        var_name() is only called when a new variable is added to the local variables.
    So rename add_value_dict() to be new_variable() and call var_name() within it.  This
    saves half a dozen lines of code.  More importantly, it bundles creating a variable
    (name) and making sure it is declared.
        Improve (fix!) many comments.

--- src/complexities_generator.py
+++ src/complexities_generator.py
@@ -3,12 +3,130 @@ Complexities Generator Module.
 
 Compose and generate the complexities that will be used by the Generator module.
 
- *modified "Tue Aug  1 14:20:21 2023" *by "Paul E. Black"
+ *modified "Mon Feb  5 16:31:38 2024" *by "Paul E. Black"
 """
 
 from jinja2 import Template, DebugUndefined
 import src.generator
 from src.synthesize_code import make_assign, get_indent
+
+class ComplexityInstance(object):
+    """
+    Complexity Instance class
+        Args:
+            **_code** (str): The code for this instance.  This is refined during
+                compliation.
+
+            **_name** (str): The code name of the function or class,
+                e.g. 'function_3' or 'Class_1'.
+
+            **complexity_type** (str): Complexity type; from ComplexitySample _type,
+                'class_traversal', or 'function_traversal'.  None if not initialized.
+
+        Attributes:
+            **_code** (str): The code for this instance.  This is refined during
+                compliation.
+
+            **_name** (str): The code name of the function or class,
+                e.g. 'function_3' or 'Class_1'.
+
+            **_type** (str): Complexity type; from ComplexitySample _type,
+                'class_traversal', or 'function_traversal'.  None if not initialized.
+
+            **_local_declarations** (dict of {str (type), set of str (var)}):
+                Programming language data types, e.g. 'int' or 'string', and a set of
+                variables to be declared of that type, e.g. 'tainted_11' or '$tainted_2'.
+    """
+
+    def __init__(self, code, name="", complexity_type=None):
+        self._code = code
+        self._name = name
+        self._type = complexity_type
+        self._local_declarations = {}
+
+    @property
+    def code(self):
+        """
+        The code for this complexity instance.  This is refined during compliation.
+
+        :getter: Return the code.
+        :type: str
+        """
+        return self._code
+
+    def set_code(self, code):
+        """
+        Set the code for this complexity.
+
+        :setter: Sets the code.
+        :type: str
+        """
+        self._code = code
+
+    @property
+    def name(self):
+        """
+        The code name of the function or class, e.g. 'function_3' or 'Class_1'.
+
+        :getter: Return the name.
+        :type: str
+        """
+        return self._name
+
+    def set_name(self, name):
+        """
+        Set the code name of the function or class, e.g. 'function_3' or 'Class_1'.
+
+        :setter: Set the name.
+        :type: str
+        """
+        self._name = name
+
+    @property
+    def complexity_type(self):
+        """
+        The complexity type; from ComplexitySample _type, 'class_traversal', or
+       'function_traversal'.  None if not initialized.
+
+        :getter: Return the complexity type.
+        :type: str
+        """
+        return self._type
+
+    def set_complexity_type(self, complexity_type):
+        """
+        Set the complexity type.  This comes from ComplexitySample _type or is
+        'class_traversal' or 'function_traversal'.
+
+        :setter: Set the complexity type.
+        :type: str
+        """
+        self._type = complexity_type
+
+    @property
+    def local_decls(self):
+        """
+        Variables to be declared.  Specifically, the programming language types used,
+        e.g. 'int' or 'string', are keys that have a set of variables of that type,
+        e.g. 'tainted_11' or '$tainted_2'.
+
+        :getter: Return the local declarations
+        :type: dict of {str (data type), set of str (var)}
+        """
+        return self._local_declarations
+
+    def add_value_dict(self, data_type, var_name):
+        """
+        Remember to declare a variable, e.g. 'tainted_11' or '$tainted_2', of the
+        code data type, e.g. 'int' or 'string'.
+
+        :setter: Add the variable name with the given data type.
+        """
+
+        local_decl_dict = self.local_decls
+        if data_type not in local_decl_dict:
+            local_decl_dict[data_type] = set()
+        local_decl_dict[data_type].add(var_name)
 
 class ComplexitiesGenerator(object):
     """
@@ -43,19 +161,15 @@ class ComplexitiesGenerator(object):
 
                 **id_var_in** (int): ID for intern variables used in composition of complexities (in).
 
-                **id_var_out** (int): ID for intern variables used in composition of cpomplexities (out).
+                **id_var_out** (int): ID for intern variables used in composition of complexities (out).
 
-                **_in_ext_name** (str): Name of external intput variable (for input) \
-                                        (private member, please use getter and setter).
+                **_in_ext_name** (str): Name of external intput variable (for input).
 
-                **_in_int_name** (str): Name of internal intput variable (for filtering) \
-                                        (private member, please use getter and setter).
+                **_in_int_name** (str): Name of internal intput variable (for filter).
 
-                **_out_ext_name** (str): Name of external output variable (for sink) \
-                                         (private member, please use getter and setter).
+                **_out_ext_name** (str): Name of external output variable (for sink).
 
-                **_out_int_name** (str): Name of internal output variable (for filtering) \
-                                         (private member, please use getter and setter).
+                **_out_int_name** (str): Name of internal output variable (for filter).
 
                 **template_code** (str): Modified template.
     """
@@ -70,18 +184,15 @@ class ComplexitiesGenerator(object):
         self.language = language
 
         self.complexities = []
-        self.complexities.append({'type': None, 'code': "{{filtering_content}}", 'local_var': {}, 'name': ""})
+        self.complexities.append(ComplexityInstance("{{filtering_content}}"))
 
         self.id_var_in = len(complexities_array)*2
         self.id_var_out = self.id_var_in+1
 
         self._in_ext_name = None
-        self._in_int_name = self.var_name(self.id_var_in)
+        self._in_int_name = self.new_variable(self.input_type, self.id_var_in)
         self._out_ext_name = None
-        self._out_int_name = self.var_name(self.id_var_out)
-
-        self.add_value_dict(self.input_type, self._in_int_name)
-        self.add_value_dict(self.output_type, self._out_int_name)
+        self._out_int_name = self.new_variable(self.output_type, self.id_var_out)
 
     @property
     def in_ext_name(self):
@@ -123,6 +234,7 @@ class ComplexitiesGenerator(object):
         """
         return self._out_int_name
 
+    @property
     def get_template(self):
         """
         Gets modified template.
@@ -140,7 +252,8 @@ class ComplexitiesGenerator(object):
 
     def compose(self):
         """
-        This method composes all complexities.
+        This method composes all complexities.  Return complexities attribute, a
+        (possibly empty) list of dict with complexities
         """
         for c in reversed(self.complexities_array):
             # check if complexities is in 2 parts (code and body)
@@ -148,7 +261,6 @@ class ComplexitiesGenerator(object):
                 t = Template(c.body, undefined=DebugUndefined)
             else:
                 t = Template(c.code, undefined=DebugUndefined)
-            # generate uid if it's needed
 
             # if function/class generate a name
             call_name = None
@@ -162,44 +274,47 @@ class ComplexitiesGenerator(object):
             in_var, out_var = self.get_in_out_var(c)
 
             # render on code
-            self.complexities[0]['code'] = t.render(placeholder=self.complexities[0]['code'], id=uid, in_var_name=in_var, out_var_name=out_var, call_name=call_name, in_var_type=self.input_type, out_var_type=self.output_type)
+            self.complexities[0].set_code(t.render(placeholder=self.complexities[0].code,
+                                        id=uid, in_var_name=in_var, out_var_name=out_var,
+                                        call_name=call_name, in_var_type=self.input_type,
+                                        out_var_type=self.output_type))
 
             # traversal for class/function where the placeholder is in the body of function/class
             if c.indirection and c.in_out_var == "traversal":
                 # LOCAL VARS
-                local_var_code = self.generate_local_var_code(self.complexities[0]['local_var'])
+                local_var_code = self.generate_local_var_code(self.complexities[0].local_decls)
                 # put local var on body
-                self.complexities[0]['code'] = Template(self.complexities[0]['code'], undefined=DebugUndefined).render(local_var=local_var_code)
+                self.complexities[0].set_code(Template(self.complexities[0].code, undefined=DebugUndefined).render(local_var=local_var_code))
                 # add in_var
                 self.id_var_in -= 1
-                in_var = self.var_name(self.id_var_in)
-                self.add_value_dict(self.input_type, in_var)
+                in_var = self.new_variable(self.input_type, self.id_var_in)
                 # add out_var
                 self.id_var_out += 1
-                out_var = self.var_name(self.id_var_out)
-                self.add_value_dict(self.output_type, out_var)
+                out_var = self.new_variable(self.output_type, self.id_var_out)
                 # change type of current complexities
-                self.complexities[0]['type'] = c.type
+                self.complexities[0].set_complexity_type(c.type)
                 if c.type == "class":
-                    self.complexities[0]['type'] = "class_traversal"
+                    self.complexities[0].set_complexity_type("class_traversal")
                 elif c.type == "function":
-                    self.complexities[0]['type'] = "function_traversal"
-                self.complexities[0]['name'] = call_name
+                    self.complexities[0].set_complexity_type("function_traversal")
+                self.complexities[0].set_name(call_name)
                 # ###################################
                 # start a new stack of complexities #
                 #####################################
                 # insert new dict for the next complexity
-                self.complexities.insert(0, {'type': None, 'code': c.code, 'local_var': {}, 'name': ""})
+                self.complexities.insert(0, ComplexityInstance(c.code))
                 t = Template(c.code, undefined=DebugUndefined)
-                self.complexities[0]['code'] = t.render(placeholder=self.complexities[0]['code'], id=uid, in_var_name=in_var, out_var_name=out_var, call_name=call_name)
-            # in case if the placeholder is before or after the call to class/function
+                self.complexities[0].set_code(
+                    t.render(placeholder=self.complexities[0].code, id=uid,
+                        in_var_name=in_var, out_var_name=out_var, call_name=call_name))
+            # if the placeholder is before or after the call to class/function
             elif c.indirection and (c.in_out_var == "in" or c.in_out_var == "out"):
                 if c.type == "class":
                     body = Template(c.body, undefined=DebugUndefined).render(id=uid, in_var_type=self.input_type, out_var_type=self.output_type, call_name=call_name)
-                    self.complexities.insert(1, {'type': "class", 'code': body, 'local_var': {}, 'name': call_name})
+                    self.complexities.insert(1, ComplexityInstance(body, complexity_type="class", name=call_name))
                 elif c.type == "function":
                     body = Template(c.body).render(id=uid, in_var_type=self.input_type, out_var_type=self.output_type, call_name=call_name)
-                    self.complexities.insert(1, {'type': "function", 'code': body, 'local_var': {}, 'name': call_name})
+                    self.complexities.insert(1, ComplexityInstance(body, complexity_type="function", name=call_name))
 
         return self.fill_template()
 
@@ -208,34 +323,32 @@ class ComplexitiesGenerator(object):
         Fill template with previous composed complexities with local var and instructions.
         """
         # name of external variable to join with input and sink
-        self._in_ext_name = self.var_name(self.id_var_in)
-        self._out_ext_name = self.var_name(self.id_var_out)
-        # add to list with local var
-        self.add_value_dict(self.input_type, self._in_ext_name)
-        self.add_value_dict(self.output_type, self._out_ext_name)
+        self._in_ext_name = self.new_variable(self.input_type, self.id_var_in)
+        self._out_ext_name = self.new_variable(self.output_type, self.id_var_out)
 
         functions_code = ""
         classes_code = []
         # compose complexity i inti i-1
         for c in reversed(self.complexities[1:]):
-            if c['type'] == "class_traversal":
+            if c.complexity_type == "class_traversal":
                 # add a new class code when we have a traversal class
                 imports_content = self.template.generate_imports(set(self.filtering.imports))
-                classes_code.append({'code': Template(c['code'], undefined=DebugUndefined).render(static_methods=functions_code, imports=imports_content), 'name': c['name']})
+                classes_code.append({'code': Template(c.code, undefined=DebugUndefined).render(static_methods=functions_code, imports=imports_content), 'name': c.name})
                 functions_code = ""
-            elif c['type'] == "class":
-                classes_code.append({'code': c['code'], 'name': c['name']})
-            elif c['type'] == "function_traversal":
-                functions_code += Template(c['code'], undefined=DebugUndefined).render(static_methods=functions_code)
-            elif c['type'] == "function":
-                functions_code += c['code'] + "\n\n"
+            elif c.complexity_type == "class":
+                classes_code.append({'code': c.code, 'name': c.name})
+            elif c.complexity_type == "function_traversal":
+                functions_code += Template(c.code, undefined=DebugUndefined).render(static_methods=functions_code)
+            elif c.complexity_type == "function":
+                functions_code += c.code + "\n\n"
 
         # generate local vars
-        local_var_code = self.generate_local_var_code(self.complexities[0]['local_var'])
-        filtering_code = self.complexities[0]['code']
+        local_var_code = self.generate_local_var_code(self.complexities[0].local_decls)
+        filtering_code = self.complexities[0].code
         t = Template(self.template.code, undefined=DebugUndefined)
         # fill template with local vars, complexities and static methods
         self.template_code = t.render(filtering_content=filtering_code, local_var=local_var_code, static_methods=functions_code)
+
         # return all classes not in the main file
         return classes_code
 
@@ -267,12 +380,14 @@ class ComplexitiesGenerator(object):
                 local_var_code += make_assign(n, init, self.template) + "\n"
         return local_var_code
 
-    def add_value_dict(self, key, value):
-        """ Adds a variable name into a dict. This dict is compile after for local var """
-        dico = self.complexities[0]['local_var']
-        if key not in dico:
-            dico[key] = set()
-        dico[key].add(value)
+    def new_variable(self, data_type, id):
+        """
+        Create a new variable with a name based on the id (number).  Store it to be
+        declared with the given data type.  Return the variable's name.
+        """
+        new_var_name = self.var_name(id)
+        self.complexities[0].add_value_dict(data_type, new_var_name)
+        return new_var_name
 
     def get_in_out_var(self, c):
         """Generated name for variable in different cases (in/traversal/out)."""
@@ -281,25 +396,19 @@ class ComplexitiesGenerator(object):
         if c.in_out_var == "in":
             # just change input var name
             self.id_var_in -= 1
-            in_var = self.var_name(self.id_var_in)
-            out_var = self.var_name(self.id_var_in+1)
-            self.add_value_dict(self.input_type, in_var)
-            self.add_value_dict(self.input_type, out_var)
+            in_var  = self.new_variable(self.input_type, self.id_var_in)
+            out_var = self.new_variable(self.input_type, self.id_var_in+1)
         elif c.in_out_var == "out":
             # just change output var name
-            in_var = self.var_name(self.id_var_out)
+            in_var = self.new_variable(self.output_type, self.id_var_out)
             self.id_var_out += 1
-            out_var = self.var_name(self.id_var_out)
-            self.add_value_dict(self.output_type, in_var)
-            self.add_value_dict(self.output_type, out_var)
+            out_var = self.new_variable(self.output_type, self.id_var_out)
         elif c.in_out_var == "traversal":
             # change input/output var name
-            in_var = self.var_name(self.id_var_in)
+            in_var = self.new_variable(self.input_type, self.id_var_in)
             self.id_var_in -= 1
-            out_var = self.var_name(self.id_var_out)
+            out_var = self.new_variable(self.output_type, self.id_var_out)
             self.id_var_out += 1
-            self.add_value_dict(self.input_type, in_var)
-            self.add_value_dict(self.output_type, out_var)
         return in_var, out_var
 
 # end of complexities_generator.py
